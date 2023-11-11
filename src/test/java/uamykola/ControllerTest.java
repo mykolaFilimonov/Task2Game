09b96dddd7ba9e07d1aedcea6911d4ef5724c705
mfilimonov <m.filimonov@finhike.com>
2023-11-11 11:40:03 +0200

    test55

--- src/test/java/uamykola/ControllerTest.java
+++ src/test/java/uamykola/ControllerTest.java
@@ -13,4 +13,13 @@ public class ControllerTest extends TestCase {
     System.out.println(model.getSoughtValue());
     controller.checkOfTurn(model.getSoughtValue());
   }
+
+  @Test
+  public void testCheckOfTurnFalse() {
+    var controller = new Controller();
+    Model model = controller.getModel();
+    model.setSoughtValue(Model.START_MAX, Model.START_MIN);
+    System.out.println(model.getSoughtValue() - 1);
+    controller.checkOfTurn(model.getSoughtValue());
+  }
 }
